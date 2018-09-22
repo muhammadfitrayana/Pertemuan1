e23e47db8e7756dacc16feeb6442ed21034f81cd
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-09-22 12:35:49 +0200

    Latihan15

---
+++ PBO3-10117116-Latihan15-Logika/src/pbo3/pkg10117116/latihan15/logika/PBO310117116Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Program Ini Berisi Program Penggunaan Operator Logika
+ */
+
+public class PBO310117116Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b ));
+        System.out.println("a || b = " + (a || b ));
+        System.out.println("!a  = " + (!a));
+        System.out.println("!(a && b) = " + !(a && b ));
+    }
+    
+}
