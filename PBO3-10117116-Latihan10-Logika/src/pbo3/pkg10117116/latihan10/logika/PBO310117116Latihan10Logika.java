0b806ec0248d4bf5380aae441af149297f2244d7
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-09-22 12:23:07 +0200

    Latihan10

---
+++ PBO3-10117116-Latihan10-Logika/src/pbo3/pkg10117116/latihan10/logika/PBO310117116Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Program Ini Berisi Program Logika untuk Mengetahui   
+ * Umur Sudah Dewasa atau Belum
+ */
+public class PBO310117116Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
