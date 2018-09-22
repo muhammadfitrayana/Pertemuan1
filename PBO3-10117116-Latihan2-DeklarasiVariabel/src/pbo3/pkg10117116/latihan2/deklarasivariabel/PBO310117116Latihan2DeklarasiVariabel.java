3ece48cd967809fe141ab6eb8a87852bc860ab10
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-09-22 12:12:04 +0200

    Latihan2

---
+++ PBO3-10117116-Latihan2-DeklarasiVariabel/src/pbo3/pkg10117116/latihan2/deklarasivariabel/PBO310117116Latihan2DeklarasiVariabel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Program Ini Berisi Program Untuk Pengaksesan dan
+ * Menampilkan Pendeklarasian Variabel
+ */
+public class PBO310117116Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    //Deklarasi Variabel
+    int nilaiInt;
+    final double PHI = 3.14;  //konstanta uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //Memberi Nilai Ke Variabel
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    // Menampilkan Hasil
+    System.out.println();
+    System.out.println("Isi Variabel nilai = " + nilaiInt);
+    System.out.println("Isi Variabel PHI = " + PHI);
+    System.out.println("Isi Variabel Logika = " + nilaiLogika);
+    System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
