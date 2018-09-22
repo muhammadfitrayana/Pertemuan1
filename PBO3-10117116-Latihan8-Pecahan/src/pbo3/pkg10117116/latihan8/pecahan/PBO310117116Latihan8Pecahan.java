c42273cb5b968d8348f404bb2547882a9b53cb0d
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-09-22 12:20:14 +0200

    Latihan8

---
+++ PBO3-10117116-Latihan8-Pecahan/src/pbo3/pkg10117116/latihan8/pecahan/PBO310117116Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Program Ini Berisi Program Untuk Mendeklarasikan dan  
+ * Menampilkan tipe data berbentuk pecahan
+ */
+public class PBO310117116Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float   : " + f);
+        System.out.println("Double  : " + d);
+    }
+    
+}
