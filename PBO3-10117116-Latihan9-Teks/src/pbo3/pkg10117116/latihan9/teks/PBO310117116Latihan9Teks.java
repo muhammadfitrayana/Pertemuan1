5daa61b9557f2bf02ac454a2e18c716d6810f72a
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-09-22 12:21:39 +0200

    Latihan9

---
+++ PBO3-10117116-Latihan9-Teks/src/pbo3/pkg10117116/latihan9/teks/PBO310117116Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan9.teks;
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Program Ini Berisi Program untuk mengeluarkan Teks dari   
+ * pendeklarasian menggunakan tipe data char
+ */
+
+public class PBO310117116Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char : " + tab + "berisi " + c);   
+    }
+    
+}
